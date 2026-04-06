cf48c929ea1ec1dc62eb15373be515fc41b648db
Link4Electronics <link4electronics@gmail.com>
2026-04-06 00:35:02 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,7 +7,7 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/256x256/apps/clownmdemu.png
+export ICON=/usr/share/icons/hicolor/scalable/apps/clownmdemu.svg
 export DESKTOP=/usr/share/applications/com.clownacy.clownmdemu.desktop
 export DEPLOY_OPENGL=1
 
