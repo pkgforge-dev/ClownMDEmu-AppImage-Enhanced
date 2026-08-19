04c7effa8680aacf05eddf1f974ac3befab31004
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:11:58 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,14 +15,9 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package
-
-# If the application needs to be manually built that has to be done down here
 echo "Building ClownMDEmu..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/Clownacy/clownmdemu-frontend"
-# Determine to build nightly or stable
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
 	echo "Making nightly build of ClownMDEmu..."
 	echo "---------------------------------------------------------------"
